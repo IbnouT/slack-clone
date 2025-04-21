aa687b7d6e0ecce2ae5eed0ab09c5ea6c193558f
IbnouT <ibnou.toure001@gmail.com>
2025-04-21 20:51:07 +0400

    feat(workspace): add workspace layout and toolbar components

--- src/app/workspace/[workspaceId]/page.tsx
+++ src/app/workspace/[workspaceId]/page.tsx
@@ -1,16 +1,23 @@
-interface WorkspacePageProps {
-    params: {
-        workspaceId: string;
-    }
-}
+"use client";
 
-const WorkspacePage = async ({ params }: WorkspacePageProps) => {
-    const { workspaceId } = await params;
+import { useGetWorkspace } from "@/features/workspaces/api/use-get-workspace";
+import { useWorkspaceId } from "@/hooks/use-workspace-id";
+import { Skeleton } from "@/components/ui/skeleton";
+const WorkspacePage = () => {
+    const workspaceId = useWorkspaceId();
+    const { data, isLoading } = useGetWorkspace({ id: workspaceId });
+
+    if (isLoading) {
+        return <Skeleton className="h-full w-full" />
+    }
+
+    if (!workspaceId) {
+        return <div>Workspace not found</div>;
+    }
 
     return (
         <div>
-            <h1>WorkspacePage</h1>
-            <p>{workspaceId}</p>
+            Data: {JSON.stringify(data)}
         </div>
     )
 }   
